367b7be592317a2beaba69330f19260e0688297d
Matthias Budde <buddemat@users.noreply.github.com>
2022-11-29 21:56:19 +0100

    Updated db and model name in app.

--- app/run.py
+++ app/run.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
@@ -26,11 +25,10 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
-
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('CategorizedMessages', engine)
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -42,7 +40,6 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
